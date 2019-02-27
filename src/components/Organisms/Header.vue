24475085c54ce1635149964218411fece94873ba
07JP27 <jynpei.t@gmail.com>
2019-02-27 23:37:36 +0900

    フォルダ構成とヘッダーの作成

---
+++ src/components/Organisms/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-toolbar dark color='#115740'>
+    <v-toolbar-items class="text-sm-left">
+      <img alt="Vue logo" src="../../assets/logo.svg">
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+  </v-toolbar>
+</template>
---
+++ src/components/Templates/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../../assets/logo.png">
+    <router-link to='/about'>About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
