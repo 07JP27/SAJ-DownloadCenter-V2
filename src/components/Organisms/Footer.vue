dcc7b5274ee8eca8f572d1b283dcf7d3e22ff5d6
07JP27 <jynpei.t@gmail.com>
2019-05-28 22:30:15 +0900

    Update Footer.vue

--- src/components/Organisms/Footer.vue
+++ src/components/Organisms/Footer.vue
@@ -1,26 +1,45 @@
 <template>
-  <v-footer dark height="auto">
-    <v-card class="flex" flat tile>
-      <v-card-title class="scoutGreen">
-        <v-layout align-center justify-space-around row fill-height>
-            <img class="footer-logo" src="../../assets/footer-logo.svg" width="40%" />
-        </v-layout>
-      </v-card-title>
-
-      <v-card-actions class="darkScoutGreen">
-          <p>&copy;2018 — Scout Association of Japan</p>
-          <v-spacer></v-spacer>
-          <p>ボーイスカウト日本連盟事務局 社会連携・広報部</p>
-      </v-card-actions>
-    </v-card>
-  </v-footer>
+  <footer class="footer">
+    <div class="scoutGreen text-center">
+      <img class="footer-logo" src="../../assets/footer-logo.svg" width="40%" >
+      <div class="row">
+        <div class="offset-md-4 col-md-4">
+          <div class="row">
+            <img class="col-md-3" src="../../assets/icon/twitter.svg">
+            <img class="col-md-3" src="../../assets/icon/facebook.svg">
+            <img class="col-md-3" src="../../assets/icon/youtube.svg">
+            <img class="col-md-3" src="../../assets/icon/instagram.svg">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="darkScoutGreen">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <p>&copy;2018 — Scout Association of Japan</p>
+          </div>
+          <div class="col text-right">
+            <p>ボーイスカウト日本連盟事務局 社会連携・広報部</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </footer>
 </template>
 
 <style>
 .scoutGreen{
-    background-color: #115740;
+  color: white;
+  background-color: #115740;
 }
 .darkScoutGreen{
-    background-color: #0B3C2C;
+  color: white;
+  background-color: #0B3C2C;
+}
+.darkScoutGreen p{
+  margin-bottom: 0;
+  padding: 0.3rem 0;
 }
 </style>
