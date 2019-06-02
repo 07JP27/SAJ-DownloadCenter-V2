08e6e01970115d0136641438b4dd422126b9263c
07JP27 <jynpei.t@gmail.com>
2019-06-02 14:37:03 +0900

    Toolページの作成

---
+++ src/components/Templates/Tool.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <b-container>
+      tool
+    </b-container>
+  </div>
+</template>
+
+<script>
+import prTools from '../../data'
+
+export default {
+  name: 'tool',
+  mixins: [ prTools ]
+}
+</script>
+
+<style scoped>
+.wrapper{
+  padding-top: 64px;
+}
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Templates/Home.vue'
+import About from '../components/Templates/About.vue'
+import Tool from '../components/Templates/Tool.vue'
 
 Vue.use(Router)
 
@@ -14,10 +16,12 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../components/Templates/About.vue')
+      component: About
+    },
+    {
+      path: '/Tool',
+      name: 'Tool',
+      component: Tool
     }
   ]
 })
